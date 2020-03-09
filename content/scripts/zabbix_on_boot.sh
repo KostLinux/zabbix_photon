c5759b4069933364e24b69a364ea458b707051c7
KostLinux <46846027+KostLinux@users.noreply.github.com>
2020-03-09 16:25:02 +0200

    Update zabbix_on_boot.sh

--- content/scripts/zabbix_on_boot.sh
+++ content/scripts/zabbix_on_boot.sh
@@ -5,6 +5,7 @@ mkdir /var/run/zabbix/
 chmod 775 /var/run/zabbix
 mkdir /var/log/zabbix/ 
 chmod 755 /var/log/zabbix
+chown -R zabbix. /var/log/zabbix /var/run/zabbix
 # Start zabbix-agent
 systemctl enable zabbix-agent
 systemctl restart zabbix-agent
